7bc94947c43514c538f7c1abf8cc19209e8bcafe
utof <bondar030@mail.ru>
2025-02-18 12:56:16 +0300

    a

---
+++ build/_deps/eigen-subbuild/eigen-populate-prefix/tmp/eigen-populate-mkdirs.cmake
@@ -0,0 +1,27 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
+
+# If CMAKE_DISABLE_SOURCE_CHANGES is set to true and the source directory is an
+# existing directory in our source tree, calling file(MAKE_DIRECTORY) on it
+# would cause a fatal error, even though it would be a no-op.
+if(NOT EXISTS "G:/0utoffiles/coding_proj_cfg_envs_scripts/repulsive_curves_implementation/build/_deps/eigen-src")
+  file(MAKE_DIRECTORY "G:/0utoffiles/coding_proj_cfg_envs_scripts/repulsive_curves_implementation/build/_deps/eigen-src")
+endif()
+file(MAKE_DIRECTORY
+  "G:/0utoffiles/coding_proj_cfg_envs_scripts/repulsive_curves_implementation/build/_deps/eigen-build"
+  "G:/0utoffiles/coding_proj_cfg_envs_scripts/repulsive_curves_implementation/build/_deps/eigen-subbuild/eigen-populate-prefix"
+  "G:/0utoffiles/coding_proj_cfg_envs_scripts/repulsive_curves_implementation/build/_deps/eigen-subbuild/eigen-populate-prefix/tmp"
+  "G:/0utoffiles/coding_proj_cfg_envs_scripts/repulsive_curves_implementation/build/_deps/eigen-subbuild/eigen-populate-prefix/src/eigen-populate-stamp"
+  "G:/0utoffiles/coding_proj_cfg_envs_scripts/repulsive_curves_implementation/build/_deps/eigen-subbuild/eigen-populate-prefix/src"
+  "G:/0utoffiles/coding_proj_cfg_envs_scripts/repulsive_curves_implementation/build/_deps/eigen-subbuild/eigen-populate-prefix/src/eigen-populate-stamp"
+)
+
+set(configSubDirs Debug)
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "G:/0utoffiles/coding_proj_cfg_envs_scripts/repulsive_curves_implementation/build/_deps/eigen-subbuild/eigen-populate-prefix/src/eigen-populate-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "G:/0utoffiles/coding_proj_cfg_envs_scripts/repulsive_curves_implementation/build/_deps/eigen-subbuild/eigen-populate-prefix/src/eigen-populate-stamp${cfgdir}") # cfgdir has leading slash
+endif()
--- readme.md
+++ readme.md
@@ -1,4 +1,7 @@
 install clang, vs2022, include clang reqs there. install ninja via chocolatey
+delete cmakecache if its in the main folder or in the build
+updated cmakelists, check it out
+ctrl+p then cmake select kit and select clang (not clang-cl) , i have ver 19 something
 cd build
 cmake ..
 cmake --build build
